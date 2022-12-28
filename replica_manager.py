5a64cd88e9991cba05fbba10cec16a27ceb49846
podmar <marta.podziewska@gmail.com>
2022-12-28 16:53:28 +0100

    create_replica copies a directory at the given path

--- replica_manager.py
+++ replica_manager.py
@@ -1,9 +1,20 @@
-# write a program that copies an existing folder into a new folder
-# apply command line arguments for paths source and replica
-# build logging functionality
-# apply copy at interval
-# apply a hashing algorithm
+# [x] write a program that copies an existing folder into a new folder
+# [] apply command line arguments for paths source and replica
+# [] build logging functionality
+# [] apply copy at interval
+# [] apply a hashing algorithm
+
+import shutil
+
 
 def create_replica(path: str) -> None:
     """A function creating a copy of an existing folder"""
+
+    # define path to the default destination
+    last_dst_index = path[:-1].rfind('/')
+    default_dst = f'{path[:last_dst_index]}/replica_{path[last_dst_index+1:]}'
+
+    # create a copy of the directory
+    shutil.copytree(path, default_dst)
+
     return

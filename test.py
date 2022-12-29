3f51cd9582c71cbe97b7569c8bea71a594d23a95
podmar <marta.podziewska@gmail.com>
2022-12-29 16:11:30 +0100

    2 new tests for destination CLI argument

--- test.py
+++ test.py
@@ -18,14 +18,22 @@ class TestReplicaCreated(TestCase):
     def setUp(self):
         self.tmp_test_container = tempfile.mkdtemp()
         self.tmp_test_src = tempfile.mkdtemp(dir=self.tmp_test_container)
+        self.tmp_test_file = tempfile.mkstemp(dir=self.tmp_test_src)
+        self.tmp_test_dst = tempfile.mkdtemp(dir=self.tmp_test_container)
 
     def tearDown(self):
         shutil.rmtree(self.tmp_test_container)
 
-    def test_replica_created_success(self):
-        """Tests that a copy of a folder is created in the same directory."""
+    def test_replica_created_default_dst_success(self):
+        """Tests that a copy of a folder is created in the same directory if no destination path is given."""
         test_replica = Replica(self.tmp_test_src)
         self.assertEqual(len(os.listdir(self.tmp_test_container)), 2)
+
+    def test_replica_created_success(self):
+        """Tests that a copy of a folder is created at the given destination path."""
+        test_replica = Replica(self.tmp_test_src, self.tmp_test_dst)
+        self.assertEqual(len(os.listdir(self.tmp_test_dst)),
+                         len(os.listdir(self.tmp_test_src)))
 
 
 class TestCliArguments(TestCase):
@@ -34,6 +42,7 @@ class TestCliArguments(TestCase):
     def setUp(self):
         self.tmp_test_container = tempfile.mkdtemp()
         self.tmp_test_src = tempfile.mkdtemp(dir=self.tmp_test_container)
+        self.tmp_test_dst = tempfile.mkdtemp(dir=self.tmp_test_container)
 
     def tearDown(self):
         shutil.rmtree(self.tmp_test_container)
@@ -42,6 +51,11 @@ class TestCliArguments(TestCase):
         with patch.object(sys, 'argv', ['replica_manager.py', self.tmp_test_src]):
             test_replica = Replica(sys.argv[1])
             self.assertEqual(test_replica.src, self.tmp_test_src)
+
+    def test_destination_argument_correct(self):
+        with patch.object(sys, 'argv', ['replica_manager.py', self.tmp_test_src, self.tmp_test_dst]):
+            test_replica = Replica(sys.argv[1], )
+            self.assertEqual(test_replica.dst, self.tmp_test_dst)
 
 
 if __name__ == '__main__':

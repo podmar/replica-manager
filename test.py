350c48c6dab6b2e31d34001584ea791f01c0c279
podmar <marta.podziewska@gmail.com>
2022-12-30 17:50:20 +0100

    refactor destination path

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ import shutil
 import tempfile
 import os
 import sys
+import time
 
 from replica_manager import Replica
 
@@ -19,15 +20,15 @@ class TestReplicaCreated(TestCase):
         self.tmp_test_container = tempfile.mkdtemp()
         self.tmp_test_src = tempfile.mkdtemp(dir=self.tmp_test_container)
         self.tmp_test_file = tempfile.mkstemp(dir=self.tmp_test_src)
-        self.tmp_test_dst = tempfile.mkdtemp(dir=self.tmp_test_container)
+        self.tmp_test_dst = f'{self.tmp_test_container}/test_replica_folder'
 
     def tearDown(self):
         shutil.rmtree(self.tmp_test_container)
 
     def test_replica_folder_created_success(self):
         """Tests that a copy of the source folder is created in the destination directory"""
-        replica_dir_path = f'{self.tmp_test_dst}/test_replica_folder'
-        test_replica = Replica(self.tmp_test_src, replica_dir_path)
+        test_replica = Replica(self.tmp_test_src, self.tmp_test_dst)
+
         self.assertEqual(len(os.listdir(self.tmp_test_dst)), 1)
 
     def test_replica_folder_contains_files(self):
@@ -36,6 +37,8 @@ class TestReplicaCreated(TestCase):
         test_replica = Replica(self.tmp_test_src, replica_dir_path)
         self.assertEqual(len(os.listdir(replica_dir_path)), 1)
 
+# TODO: test that if a replica not created an exception is raised
+
 
 class TestCliArguments(TestCase):
     """Test that CLI arguments can be passed successfully."""
@@ -43,22 +46,21 @@ class TestCliArguments(TestCase):
     def setUp(self):
         self.tmp_test_container = tempfile.mkdtemp()
         self.tmp_test_src = tempfile.mkdtemp(dir=self.tmp_test_container)
-        self.tmp_test_dst = tempfile.mkdtemp(dir=self.tmp_test_container)
+        self.tmp_test_dst = f'{self.tmp_test_container}/test_replica_folder'
 
     def tearDown(self):
         shutil.rmtree(self.tmp_test_container)
 
     def test_source_argument_correct(self):
-        replica_dir_path = f'{self.tmp_test_dst}/test_replica_folder'
-        with patch.object(sys, 'argv', ['replica_manager.py', self.tmp_test_src, replica_dir_path]):
+        with patch.object(sys, 'argv', ['replica_manager.py', self.tmp_test_src, self.tmp_test_dst]):
             test_replica = Replica(sys.argv[1], sys.argv[2])
             self.assertEqual(test_replica.src, self.tmp_test_src)
 
     def test_destination_argument_correct(self):
         replica_dir_path = f'{self.tmp_test_dst}/test_replica_folder'
-        with patch.object(sys, 'argv', ['replica_manager.py', self.tmp_test_src, replica_dir_path]):
+        with patch.object(sys, 'argv', ['replica_manager.py', self.tmp_test_src, self.tmp_test_dst]):
             test_replica = Replica(sys.argv[1], sys.argv[2])
-            self.assertEqual(test_replica.dst, replica_dir_path)
+            self.assertEqual(test_replica.dst, self.tmp_test_dst)
 
 
 if __name__ == '__main__':
